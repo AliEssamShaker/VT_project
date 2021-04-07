fefbea00299f9293fca7db6d7cfc641175535fab
Ali Shaker <ali.shaker.tech@gmail.com>
2021-04-07 17:20:17 -0600

    testing fetching data from database ! finally git add .

---
+++ backend/routes/blogRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Blog from '../models/blogModel.js'
+
+
+// To fetch blogs
+// GET /api/blogs
+
+router.get('/', asyncHandler(async(req, res) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)   
+}))
+
+// To fetch a single blog
+// GET /api/blogs/:id
+router.get('/:id', asyncHandler(async(req, res) => {
+    const blog = await Blog.findById(req.params.id)
+
+        if (blog){
+            res.json(blog)
+        } else{
+            res.status(404).json({message: 'Sorry ! Blog not found !'})
+        }
+
+
+    res.json(blog)   
+}))
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import blogs from './data/blogs.js'
 
-
+import blogRoutes from './routes/blogRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -14,14 +13,8 @@ app.get('/', (req, res) => {
     res.send('API is runnning')
 })
 
-app.get('/api/blogs', (req, res) => {
-    res.json(blogs)   
-})
 
-app.get('/api/blogs/:id', (req, res) => {
-    const blog = blogs.find( b => b._id === req.params.id)
-    res.json(blog)   
-})
+app.use('/api/blogs', blogRoutes)
 
 const PORT = process.env.PORT || 5000
 
