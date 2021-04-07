a917cd1b5a2cb681b3c37c313ed3b12751c775b9
Ali Shaker <ali.shaker.tech@gmail.com>
2021-04-07 09:42:56 -0600

    added few things to the blogscreen component feature

--- frontend/src/screens/BlogScreen.js
+++ frontend/src/screens/BlogScreen.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Row, Col, Image, ListGroup} from 'react-bootstrap'
+import blogs from '../blogs'
 
-const BlogScreen = () => {
+
+const BlogScreen = ({match}) => {
+
+    const blog = blogs.find((b) => b._id === match.params.id)
+    
     return (
-        <div>
-            blog
-        </div>
+        <>
+            <Link className='btn btn-light my-3' to='/'> 
+                Go Back 
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={blog.image} alt={blog.name} fluid/>
+                </Col>
+
+                <Col>
+                    <ListGroup variant='flush'>
+                        <h3>{blog.name}</h3>
+                    </ListGroup>
+                
+                </Col>
+            </Row>
+        </>
     )
 }
 
